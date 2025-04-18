7281e032a06e79802c3009f8240a4515430fc831
kishlay <kishlay141@gmail.com>
2025-04-18 12:00:21 +0530

    installed tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // 👈 Tells Tailwind to scan your components
+  ],
+  theme: {
+    extend: {
+      // We'll put custom fonts, colors, etc. here later
+    },
+  },
+  plugins: [],
+};
