5bf5638c869e39f9e5f60db9746d354163455b62
kishlay <kishlay141@gmail.com>
2025-04-18 14:26:18 +0530

    updated the home section

--- src/components/Home/Home2.jsx
+++ src/components/Home/Home2.jsx
@@ -34,47 +34,39 @@ function Home2() {
                 exit="hidden"
                 variants={fadeInUp}
               >
-                <Row>
-                  <Col md={8} className="home-about-description">
-                    <h1 style={{ fontSize: "2.6em" }}>
-                      LET ME <span className="purple"> INTRODUCE </span> MYSELF
-                    </h1>
-                    <p className="home-about-body">
-                      I fell in love with programming and I have at least learnt
-                      something, I think… 🤷‍♂️
-                      <br />
-                      <br />I am fluent in classics like
-                      <i>
-                        <b className="purple"> C++, Javascript and Go. </b>
-                      </i>
-                      <br />
-                      <br />
-                      My field of Interest's are building new&nbsp;
-                      <i>
-                        <b className="purple">Web Technologies and Products </b>{" "}
-                        and also in areas related to{" "}
-                        <b className="purple">Blockchain.</b>
-                      </i>
-                      <br />
-                      <br />
-                      Whenever possible, I also apply my passion for developing
-                      products with <b className="purple">Node.js</b> and
-                      <i>
-                        <b className="purple">
-                          {" "}
-                          Modern Javascript Library and Frameworks
-                        </b>
-                      </i>
-                      &nbsp; like
-                      <i>
-                        <b className="purple"> React.js and Next.js</b>
-                      </i>
-                    </p>
-                  </Col>
-                  <Col md={4} className="myAvtar">
-                    <Tilt>
-                      <img src={myImg} className="img-fluid" alt="avatar" />
-                    </Tilt>
+                <Row className="justify-content-center home-intro-section">
+                  <Col md={10}>
+                    <div className="intro-box">
+                      <h1 className="intro-heading">
+                        <span className="fade-line">LET ME</span>
+                        <br />
+                        <span className="purple fade-line">INTRODUCE</span>
+                        <br />
+                        <span className="fade-line">MYSELF</span>
+                      </h1>
+
+                      <p className="intro-body">
+                        I fell in love with programming and I’ve learned a lot
+                        along the way. 🤷‍♂️
+                        <br />
+                        <br />
+                        I’m fluent in:{" "}
+                        <span className="purple">C++, JavaScript, and Go.</span>
+                        <br />
+                        <br />I enjoy building modern{" "}
+                        <span className="purple">
+                          Web Technologies and Products
+                        </span>
+                        , and I’m deeply curious about{" "}
+                        <span className="purple">Blockchain</span> and its
+                        potential.
+                        <br />
+                        <br />I also bring my ideas to life using{" "}
+                        <span className="purple">Node.js</span> and cutting-edge
+                        tools like <span className="purple">React.js</span> and{" "}
+                        <span className="purple">Next.js</span>.
+                      </p>
+                    </div>
                   </Col>
                 </Row>
 
